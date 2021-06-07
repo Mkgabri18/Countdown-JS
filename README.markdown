8672a7cddad91fe0be4670ba0ab526fc448049f0
Gabriel Carrasco <gabriel136@hotmail.it>
2021-06-07 19:14:20 +0200

    first commint of countdown js

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Setting coundown
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/mkgbri18/pen/pobzqxa](https://codepen.io/mkgbri18/pen/pobzqxa).
+
+
---
+++ src/script.js
@@ -0,0 +1,49 @@
+//base element
+const clock = document.getElementById("countdown");
+
+//get timing block
+const dayBlock = document.querySelector("block-days");
+const hourBlock = document.querySelector("block-hours");
+const minuteBlock = document.querySelector("block-min");
+const secondBlock = document.querySelector("block-sec");
+
+//consts
+const second = 1000 * 60;
+const minute = second * 60;
+const hour = minute * 24;
+
+const dateBlock = document.querySelector(".date-input");
+let selectDate = dateBlock.value;
+
+let x;
+
+function sendDate() {
+  // Data selezionata
+  selectDate = new Date(dateBlock.value);
+  selectDate.setHours(0);
+  selectDate.setMinutes(0);
+  selectDate.setSeconds(0);
+  //Devo resettare il timer precedente
+  clearTimeout(x);
+  //Richiamo funzione countdown ogni secondo
+  x = setInterval(CountDown, 1000, selectDate);
+}
+
+function CountDown(countDownDate) {
+  // Oggi
+  const now = new Date();
+  // Calcolo differenza
+  const differenceDate = new Date(countDownDate).getTime() - now;
+
+  const count = {
+    days: Math.floor(differenceDate / hour),
+    hours: Math.floor((differenceDate % hour) / minute),
+    minutes: Math.floor((differenceDate % minute) / second),
+    seconds: Math.floor((differenceDate % second) / 1000)
+  };
+
+  document.getElementById("d").innerHTML = JSON.stringify(count.days);
+  document.getElementById("h").innerHTML = JSON.stringify(count.hours);
+  document.getElementById("m").innerHTML = JSON.stringify(count.minutes);
+  document.getElementById("s").innerHTML = JSON.stringify(count.seconds);
+}
